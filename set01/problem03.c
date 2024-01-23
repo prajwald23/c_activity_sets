9053505629645990a7e5fa81d83b487e9995d367
prajwald23 <141308061+prajwald23@users.noreply.github.com>
2024-01-23 10:24:13 +0530

    test

---
+++ set01/problem03.c
@@ -0,0 +1,33 @@
+//Write a C program to add two numbers using **pass by value**
+//***Function declarations***
+//int input();
+//int add(int a, int b);
+//void output(int a, int b, int sum);
+
+#include <stdio.h>
+int input();
+int add(int a, int b);
+void output(int a, int b, int sum);
+int main() 
+{
+    printf("Enter the first number: ");
+    int a = input();
+    printf("Enter the second number: ");
+    int b = input();
+    int sum = add(a, b);
+    output(a, b, sum);
+}
+int input()
+{
+    int x;
+    scanf("%d", &x);
+    return x;
+}
+int add(int a, int b)
+{
+    return (a + b);
+}
+void output(int a, int b, int sum)
+{
+    printf("The sum of %d and %d is %d\n", a, b, sum);
+}
